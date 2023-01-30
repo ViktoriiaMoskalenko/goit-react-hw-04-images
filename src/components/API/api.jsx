70626165d7356e44d469e6749fa917afad3c3737
Viktoriia Moskalenko <vika01mos@gmail.com>
2023-01-30 02:09:03 +0200

    .

--- src/components/API/api.jsx
+++ src/components/API/api.jsx
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'https://pixabay.com/api/?key=16686655-4ef7ec615da889351893ebc22&image_type=photo&orientation=horizontal'
 export const addImages = async (value, page = 1) => {
-    const response = await axios.get(`&q=${value}&page=${page}&per_page=100`)
+    const response = await axios.get(`&q=${value}&page=${page}&per_page=20`)
     return response.data
 }
